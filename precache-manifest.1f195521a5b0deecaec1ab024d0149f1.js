84b238e6f5b216b75b6337ec6c826b3fe9ed24d6
Ali Haidar <alihaidar2950@gmail.com>
2020-08-03 12:35:27 -0400

    Updates

---
+++ precache-manifest.1f195521a5b0deecaec1ab024d0149f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466f97705f81077270906c7f7ac9e008",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2a894a7de66d972782b1",
+    "url": "/monsters-rolodex/static/css/main.6ad57746.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a894a7de66d972782b1",
+    "url": "/monsters-rolodex/static/js/main.de972fc9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
